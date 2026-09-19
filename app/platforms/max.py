a861942e3e645265017d556a6c32983f5dad98a2
Aleksandr Basov <sasha4ka99991@gmail.com>
2026-09-19 18:18:04 +0300

    remove not used settings

--- app/platforms/max.py
+++ app/platforms/max.py
@@ -1,23 +1,24 @@
 import asyncio
+import logging
 from datetime import UTC, datetime
 
 from maxapi import Bot, Dispatcher
 from maxapi.enums import ChatType
-from maxapi.types import BotStarted, MessageCreated
+from maxapi.types import BotAdded, BotStarted, MessageCreated
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from app.abc import BasePlatform
 from app.message_router import message_router
 from app.models import IngoingMessage, OutgoingMessage
 
+logging.getLogger("dispatcher").setLevel(logging.ERROR)
+
 
 class MAXSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file="dev.env", extra="ignore")
 
     max_api_token: str
-    max_admin_id: int
-
-    max_use_cert: bool = True
+    max_admin_id: int | None = None
 
 
 class MAXPlatform(BasePlatform):
@@ -26,14 +27,14 @@ class MAXPlatform(BasePlatform):
         self.bot = Bot(token=self.settings.max_api_token)
         self.dispatcher = Dispatcher()
 
-    async def _handle_incoming_message(self, event: MessageCreated):
+    async def _incoming_message_handler(self, event: MessageCreated):
         message = event.message
 
         if message.sender is None:
             return
 
         message_content = (message.body.text or "") if message.body else ""
-        timestamp = datetime.fromtimestamp(message.timestamp, UTC)
+        timestamp = datetime.fromtimestamp(message.timestamp / 1000, UTC)
 
         chat_name: str = "Direct"
 
@@ -56,22 +57,40 @@ class MAXPlatform(BasePlatform):
     async def _send_message(self, outgoing_message: OutgoingMessage):
         await self.bot.send_message(
             chat_id=outgoing_message["chat_id"],
-            text=outgoing_message["content"],
+            text=outgoing_message["content"] + outgoing_message["mark"],
         )
+
+    async def _bot_started_handler(self, event: BotStarted):
+        await self.bot.send_message(
+            chat_id=event.chat_id,
+            text=f"Привет. Это max2tg bridge. ID чата: {event.chat_id}",
+        )
+
+    async def _bot_added_to_group_handler(self, event: BotAdded):
+        if event.chat is None:
+            return
+
+        text: str
+
+        match event.chat.type:
+            case ChatType.CHANNEL:
+                text = f"New channel (wip): {event.chat_id}"
+            case ChatType.CHAT:
+                text = f"New group: {event.chat_id}"
+            case ChatType.DIALOG:
+                text = f"New direct: {event.chat_id}"
+
+        await self.bot.send_message(chat_id=self.settings.max_admin_id, text=text)
 
     async def start_handling(self):
         message_router.register_handler("max", self._send_message)
 
-        @self.dispatcher.bot_started
-        async def bot_started(event: BotStarted):
-            await self.bot.send_message(
-                chat_id=event.chat_id,
-                text=f"Привет. Это max2tg bridge. ID чата: {event.chat_id}",
-            )
+        self.dispatcher.bot_started.register(self._bot_started_handler)
 
-        @self.dispatcher.message_created
-        async def default_message(event: MessageCreated):
-            await self._handle_incoming_message(event)
+        self.dispatcher.message_created.register(self._incoming_message_handler)
+
+        if self.settings.max_admin_id is not None:
+            self.dispatcher.bot_added.register(self._bot_added_to_group_handler)
 
         try:
             await self.dispatcher.start_polling(self.bot)
